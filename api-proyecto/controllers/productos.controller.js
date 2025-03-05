765ffcfb26f4aa442da276991d06273cf62ac2cd
Juan Carlos Sanchez aguirre <juansanchezaguirre7@gmail.com>
2025-03-05 13:55:26 -0600

    rutas,controller

--- api-proyecto/controllers/productos.controller.js
+++ api-proyecto/controllers/productos.controller.js
@@ -1,8 +1,27 @@
 async function createProducto(req, res) {
-    res.status(200).send({ message: 'crear productos' });
+    res.status(200).send({ message: 'crear productos' })
 
 }
 
+async function getProducto(req, res) {
+    res.json("ver productos")
+
+}
+async function delProducto(req, res) {
+    res.status(200).send({ message: 'borrar productos' })
+
+
+}
+async function updateProducto(req, res) {
+    res.status(200).send({ message: 'actualizar productos' })
+
+
+}
+
+
 module.exports = {
-    createProducto
+    createProducto,
+    getProducto,
+    delProducto,
+    updateProducto
 }
--- api-proyecto/routes/Productos.routes.js
+++ api-proyecto/routes/Productos.routes.js
@@ -5,5 +5,8 @@ const productosController = require('../controllers/productos.controller');
 const api= express.Router();
 
 api.post("/createProduct", productosController.createProducto);
+api.get("/getProducto", productosController.getProducto);
+api.put("/updateProducto/:id", productosController.updateProducto);
+api.delete("/delProducto/:id", productosController.delProducto);
 
 module.exports = api;
